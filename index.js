f9784dc6a9e03090c96bf7ab2e7ecc98486f8de1
eanp <awifamz@gmail.com>
2023-12-22 10:06:40 +0700

    push notif

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ import React,{Component} from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import storage from "./src/storages/store"
+import { LogLevel, OneSignal } from 'react-native-onesignal';
 
 const {store, persistor} = storage()
+
+const AppID = "edc29384-96d4-4dd3-9a17-32e80c8a8a74"
 class App extends Component{
+	async componentDidMount(){
+		OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+		OneSignal.initialize(AppID);
+		OneSignal.Notifications.requestPermission(true);
+		OneSignal.Notifications.addEventListener('click', (event) => {
+			console.log('OneSignal: notification clicked:', event);
+		  });
+	}
 	render(){
 		return(
 			<Provider store={store}>
